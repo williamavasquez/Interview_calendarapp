9d85e6548ab27e93615e2fc2b168654baafab363
williamavasquez <william.a.vasquez@gmail.com>
2016-09-08 21:41:57 -0400

    Jaws added and deployed to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,33 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
 var mysql = require('mysql');
 
+// we placed the connections in this source object
+var source = {
+    // localhost
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "1123581321w",
+        database: "willinterview"
+    },
 
-// Database connection details
-var connection = mysql.createConnection({
-    port:3306,
-    host:'localhost',
-    user:'root',
-    password: '1123581321w',
-    database: 'willinterview'
-});
+    // jawsDB
+    jawsDB: {
+        port: 3306,
+        host: 'uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'x3frb4bh33iiqzfi',
+        password: "u92w0nxvi59ql1yn",
+        database: "z0orxyvg23zov0z2"
+    }
+}
+
+// we use source.[name of connection] to hook into mysql
+var connection = mysql.createConnection(source.jawsDB);
+
 
 connection.connect(function(err) {
     if (err) {
